815c23dea7f9cede9160945a70e86ddfdec16704
Roberto Lopez <roberto.lopez736146@gmail.com>
2024-12-17 20:03:53 +0000

    made a todo list in contoller and did get all message to see if i undewrtand how it needs to be done (may need to work oit later i case it fails)

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,19 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.service.AccountService;
+import com.example.service.MessageService;
+import com.example.entity.Message;
+
+import java.util.List;
+
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +21,33 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private MessageService messageService;
+
+    //TODO: Process new user resgestration
+
+    //TODO: Process user login
+
+    //TODO: Process creation of new mesage
+
+    //TODO: Process of getting all messages
+    @GetMapping("/messages")
+    public ResponseEntity<List<Message>> getAllMessages(){
+        List<Message> messages = messageService.getMessages();
+        return new ResponseEntity<>(messages, HttpStatus.OK);
+    }
+
+    //TODO: Process of getting message by messageID
+
+    //TODO: Process of deleting message by messageID
+
+    //TODO: Process of updating message by messageID
+
+    //TODO: Process of getting all meassages by accountID
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,25 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.repository.MessageRepository;
+import com.example.entity.Message;
+
+import java.util.List;
+
+@Service
 public class MessageService {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Message createMessage(String Message_Text){
+        if ((Message_Text.isBlank() == true || Message_Text.length() > 255)) return null;
+        else{
+            return null;
+        }
+    }
+
+    public List<Message> getMessages (){
+        return messageRepository.findAll();
+    }
 }
